71ee9309c74c48a13f9737db2c70d652caa10721
rag29 <rag29@njit.edu>
2016-08-19 19:48:45 -0400

    Scrolling background and some finished assets in game

---
+++ Assets/BackgroundScroll.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	public float timer;
+
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = 0f;
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		transform.position += Vector3.down * Time.deltaTime;
+
+		if (timer > 20.2f) 
+		{
+			transform.position = startPos;
+			timer = 0f;
+		}
+
+
+	}
+}
